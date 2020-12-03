c5cc5fe5192706341bd8fc5d387a973b6b699356
Marc Fernandez <marc@thanx.com>
2020-12-02 23:14:22 -0800

    Update docs.

--- docs/scripts/sourceset_dependencies.js
+++ docs/scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies = '{":sdk:dokkaHtml/androidTestRelease": [], ":sdk:dokkaHtml/androidTestSandbox": [], ":sdk:dokkaHtml/androidTestStaging": [], ":sdk:dokkaHtml/debug": [], ":sdk:dokkaHtml/main": [], ":sdk:dokkaHtml/release": [], ":sdk:dokkaHtml/sandbox": [], ":sdk:dokkaHtml/staging": []}'
+sourceset_dependencies = '{":sdk:dokkaHtml/Android": []}'
